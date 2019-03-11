82c30bafcd3ce56f5315dd17da71e2ee39e3a192
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-11 20:48:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if (int % 3 == 0) && (int % 5 == 0)
     "FizzBuzz"
-  elsif (int % 3 == 0) && (int % 5 != 0)
+  elsif int % 3 == 0
     "Fizz"
-  else (int % 5 == 0) && (int % 3 != 0)
+  else int % 5 == 0
     "Buzz"
   end
 end
